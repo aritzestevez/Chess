1ef0a0d50f7578ddda402d6d43cb190ba5c891d4
Aritz Estevez <ariestevev@gmail.com>
2024-10-31 08:24:52 +0100

    .

--- chess/src/main/java/mondragon/edu/objects/piece.java
+++ chess/src/main/java/mondragon/edu/objects/piece.java
@@ -2,10 +2,43 @@ package mondragon.edu.objects;
 
 public abstract class piece {
 
+    int id;
     int x;
     int y;
     Color color;
 
+    
+    public piece(int id, int x, int y, Color color) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public Color getColor() {
+        return color;
+    }
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
 
 
 
