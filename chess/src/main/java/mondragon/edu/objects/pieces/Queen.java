fa1f3e1eee7386264c280bd81249a4cf5d946384
Aritz Estevez <ariestevev@gmail.com>
2024-10-31 08:42:25 +0100

    changes

--- chess/src/main/java/mondragon/edu/objects/pieces/Queen.java
+++ chess/src/main/java/mondragon/edu/objects/pieces/Queen.java
@@ -2,11 +2,12 @@ package mondragon.edu.objects.pieces;
 
 import mondragon.edu.objects.Color;
 import mondragon.edu.objects.Piece;
+import mondragon.edu.objects.Position;
 
 public class Queen extends Piece{
 
-    public Queen(int id, int x, int y, Color color) {
-        super(id, x, y, color);
+    public Queen(int id, Position pos, Color color) {
+        super(id, pos, color);
     }
     
 }
