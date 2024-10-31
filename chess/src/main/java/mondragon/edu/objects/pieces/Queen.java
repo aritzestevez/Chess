3ba25ceb8bec2b25654975b117ca868de9b20685
Marco Martinez <marcomtzsantano@gmail.com>
2024-10-31 08:52:40 +0100

    variable update

--- chess/src/main/java/mondragon/edu/objects/pieces/Queen.java
+++ chess/src/main/java/mondragon/edu/objects/pieces/Queen.java
@@ -5,10 +5,7 @@ import mondragon.edu.objects.Piece;
 import mondragon.edu.objects.Position;
 
 public class Queen extends Piece{
-    int id;
-    Position position;
-    Color color;
-
+    
     public Queen(int id, Position pos, Color color) {
         super(id, pos, color);
     }
