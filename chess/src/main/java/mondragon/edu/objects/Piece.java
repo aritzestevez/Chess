8cc6d262dfd07c8c685483bdc6830f0bc60ad265
Aritz Estevez <ariestevev@gmail.com>
2024-10-31 09:07:05 +0100

    .

--- chess/src/main/java/mondragon/edu/objects/Piece.java
+++ chess/src/main/java/mondragon/edu/objects/Piece.java
@@ -1,37 +1,33 @@
 package mondragon.edu.objects;
 
+import java.util.List;
+
 public abstract class Piece {
-    
+
     Position position;
     int id;
     Color color;
 
-    
     public Piece(int id, Position position, Color color) {
         this.id = id;
         this.position = position;
         this.color = color;
     }
 
-
     public Color getColor() {
         return color;
     }
+
     public void setColor(Color color) {
         this.color = color;
     }
 
-
-    public boolean validMovement(Position pos){
-
-        Boolean isvalid = true;
-        return isvalid;
-    }
+    public abstract List<Square> getLegalMoves(ChessBoard b);
 
     @Override
     public String toString() {
         // TODO Auto-generated method stub
         return super.toString();
     }
-    
+
 }
--- chess/src/main/java/mondragon/edu/objects/pieces/Queen.java
+++ chess/src/main/java/mondragon/edu/objects/pieces/Queen.java
@@ -1,8 +1,12 @@
 package mondragon.edu.objects.pieces;
 
+import java.util.List;
+
+// import mondragon.edu.objects.ChessBoard;
 import mondragon.edu.objects.Color;
 import mondragon.edu.objects.Piece;
 import mondragon.edu.objects.Position;
+// import mondragon.edu.objects.Square;
 
 public class Queen extends Piece{
 
@@ -10,4 +14,14 @@ public class Queen extends Piece{
         super(id, pos, color);
     }
     
+    public void move(){
+        
+    }
+
+    // @Override
+    // public List<Square> getLegalMoves(ChessBoard b) {
+    //     // TODO Auto-generated method stub
+    //     throw new UnsupportedOperationException("Unimplemented method 'getLegalMoves'");
+    // };
+
 }
