76b45e815e9529c6bd4e0a01b3b5e20c3661187a
Marco Martinez <marcomtzsantano@gmail.com>
2024-10-31 08:37:41 +0100

    Position update

--- chess/src/main/java/mondragon/edu/objects/piece.java
+++ chess/src/main/java/mondragon/edu/objects/piece.java
@@ -2,31 +2,18 @@ package mondragon.edu.objects;
 
 public abstract class Piece {
     
-    int x;
-    int y;
+    Position position;
     int id;
     Color color;
 
     
-    public Piece(int id, int x, int y, Color color) {
+    public Piece(int id, Position position, Color color) {
         this.id = id;
-        this.x = x;
-        this.y = y;
+        this.position = position;
         this.color = color;
     }
 
-    public int getX() {
-        return x;
-    }
-    public void setX(int x) {
-        this.x = x;
-    }
-    public int getY() {
-        return y;
-    }
-    public void setY(int y) {
-        this.y = y;
-    }
+
     public Color getColor() {
         return color;
     }
--- chess/src/main/java/mondragon/edu/objects/pieces/Pawn.java
+++ chess/src/main/java/mondragon/edu/objects/pieces/Pawn.java
@@ -2,11 +2,12 @@ package mondragon.edu.objects.pieces;
 
 import mondragon.edu.objects.Color;
 import mondragon.edu.objects.Piece;
+import mondragon.edu.objects.Position;
 
 public class Pawn extends Piece {
 
-    public Pawn(int id, int x, int y, Color color) {
-        super(id, x, y, color);
+    public Pawn(int id, Position position, Color color) {
+        super(id,position, color);
     }
-    
+
 }
