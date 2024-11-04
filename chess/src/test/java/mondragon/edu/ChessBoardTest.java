31a467c0856e2579e4e5c9f2cc839ce5df45d942
Aritz Estevez <ariestevev@gmail.com>
2024-11-04 09:58:26 +0100

    pull

--- chess/src/test/java/mondragon/edu/ChessBoardTest.java
+++ chess/src/test/java/mondragon/edu/ChessBoardTest.java
@@ -1,6 +1,7 @@
 package mondragon.edu;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
+
 import java.lang.reflect.Method;
 
 import org.junit.Before;
@@ -12,16 +13,30 @@ public class ChessBoardTest {
     ChessBoard board;
 
     @Before
-    public void setup(){
+    public void setup() {
         board = new ChessBoard();
     }
 
     @Test
-    public void testInitializePieces() throws Exception{
+    public void testInitializePieces() throws Exception {
         Method method = ChessBoard.class.getDeclaredMethod("initializePieces");
         method.setAccessible(true);
-        
+
         method.invoke(board);
         assertNotNull(board);
+    }
+
+    @Test
+    public void testVisualizePieces() throws Exception {
+        ChessBoard Chessboard = new ChessBoard();
+        String result = "♜ ♞ ♝ ♛ ♚ ♝ ♞ ♜ \r\n" + //
+                "♟ ♟ ♟ ♟ ♟ ♟ ♟ ♟ \r\n" + //
+                "\r\n" + //
+                "\r\n" + //
+                "\r\n" + //
+                "\r\n" + //
+                "♙ ♙ ♙ ♙ ♙ ♙ ♙ ♙ \r\n" + //
+                "♖ ♘ ♗ ♕ ♔ ♗ ♘ ♖ \r\n";
+        assertEquals(result, Chessboard.toString());
     }
 }
