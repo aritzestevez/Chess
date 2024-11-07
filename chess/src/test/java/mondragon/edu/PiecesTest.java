689f2d57270f75d3fc9f830525cfc1cf5e4d6592
Amaia Bartolomé <amaiabart@gmail.com>
2024-11-07 11:16:49 +0100

    pawn, queen, rook test

--- chess/src/test/java/mondragon/edu/PiecesTest.java
+++ chess/src/test/java/mondragon/edu/PiecesTest.java
@@ -14,9 +14,6 @@ import mondragon.edu.objects.Position;
 import mondragon.edu.objects.pieces.Bishop;
 import mondragon.edu.objects.pieces.King;
 import mondragon.edu.objects.pieces.Knight;
-import mondragon.edu.objects.pieces.Pawn;
-import mondragon.edu.objects.pieces.Queen;
-import mondragon.edu.objects.pieces.Rook;
 
 public class PiecesTest {
     
@@ -75,49 +72,5 @@ public class PiecesTest {
         Knight knight = new Knight(position, Color.BLACK);
         ChessBoard chessBoard = new ChessBoard();
         assertEquals(false, knight.isValidMove(position, chessBoard.getChessboard()));
-    }
-    @Test
-    public void testPawnGetSet(){
-        Pawn pawn = new Pawn(position, Color.BLACK);
-        assertEquals(pawn.getPosition(), position);
-        assertEquals(Color.BLACK,pawn.getColor());
-    }
-    @Test
-    public void testPawnValidMovementFalse(){
-        Pawn pawn = new Pawn(position, Color.BLACK);
-        ChessBoard chessBoard = new ChessBoard();
-        assertEquals(false, pawn.isValidMove(position, chessBoard.getChessboard()));
-    }
-    @Test
-    public void testQueenGetSet(){
-        Queen queen = new Queen(position, Color.BLACK);
-        assertEquals(queen.getPosition(), position);
-        assertEquals(Color.BLACK,queen.getColor());
-    }
-    @Test
-    public void testQueenValidMovementFalse(){
-        Queen queen = new Queen(position, Color.BLACK);
-        ChessBoard chessBoard = new ChessBoard();
-        assertEquals(false, queen.isValidMove(position, chessBoard.getChessboard()));
-    }
-    @Test
-    public void testRookGetSet(){
-
-        Rook rook = new Rook(position, Color.BLACK);
-        assertEquals(rook.getPosition(), position);
-        assertEquals(Color.BLACK,rook.getColor());
-    }
-    @Test
-    public void testRookValidMovementFalse(){
-        Rook rook = new Rook(position, Color.BLACK);
-        ChessBoard chessBoard = new ChessBoard();
-        assertEquals(false, rook.isValidMove(position, chessBoard.getChessboard()));
-    }
-    @Test
-    public void testPosition(){
-        Rook rook = new Rook(position, null);
-        rook.setPosition(new Position(1, 1));
-        assertEquals(1, rook.getPosition().getX());
-        assertEquals(1, rook.getPosition().getY());
     }
 }
--- chess/src/test/java/mondragon/edu/pieces/RookTest.java
+++ chess/src/test/java/mondragon/edu/pieces/RookTest.java
@@ -1,5 +1,43 @@
 package mondragon.edu.pieces;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import mondragon.edu.objects.ChessBoard;
+import mondragon.edu.objects.Color;
+import mondragon.edu.objects.Position;
+import mondragon.edu.objects.pieces.Rook;
+
 public class RookTest {
-    
+    Position position;
+
+    @Before
+    public void setup(){
+        position = new Position(0, 0);
+    }
+
+    @Test
+    public void testRookGetSet(){
+
+        Rook rook = new Rook(position, Color.BLACK);
+        assertEquals(rook.getPosition(), position);
+        assertEquals(Color.BLACK,rook.getColor());
+    }
+
+    @Test
+    public void testRookValidMovementFalse(){
+        Rook rook = new Rook(position, Color.BLACK);
+        ChessBoard chessBoard = new ChessBoard();
+        assertEquals(false, rook.isValidMove(position, chessBoard.getChessboard()));
+    }
+
+    @Test
+    public void testPosition(){
+        Rook rook = new Rook(position, null);
+        rook.setPosition(new Position(1, 1));
+        assertEquals(1, rook.getPosition().getX());
+        assertEquals(1, rook.getPosition().getY());
+    }
 }
